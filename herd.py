64065f8644daf7e90ae7b4d1bef510a4d0fd69cb
AmaechiMcMillan <86973092+AmaechiMcMillan@users.noreply.github.com>
2021-07-17 23:02:11 -0700

    herd updated with names and dinosaur.py imported

--- herd.py
+++ herd.py
@@ -2,4 +2,14 @@ from dinosaur import Dinosaur
 
 class Herd:
     def __init__(self):
-        pass
+        self.dinosaurs = []
+        self.create_fleet()
+    
+    def create_fleet(self):
+        dinosaur1 = Dinosaur('Chicken')
+        dinosaur2 = Dinosaur('Godzilla')
+        dinosaur3 = Dinosaur('Tyrant')
+
+        self.dinosaurs.append(dinosaur1)
+        self.dinosaurs.append(dinosaur2)
+        self.dinosaurs.append(dinosaur3)
